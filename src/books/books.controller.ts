42350797b62f47a779a4c5ed0271e53aaf30daaa
hasanardhian8 <hasanardhian11@gmail.com>
2023-09-20 15:20:09 +0700

    day3

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -6,8 +6,12 @@ import {
   Post,
   Put,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { FilterBookDto } from './dto/filter-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -17,6 +21,11 @@ export class BooksController {
   @Get()
   getAllBooks() {
     return this.booksService.getAllBook();
+  }
+
+  @Get()
+  getBooks(@Query() filter: FilterBookDto) {
+    return this.booksService.getBooks(filter);
   }
 
   // @Get()
@@ -29,27 +38,19 @@ export class BooksController {
   // }
 
   @Get('/:id')
-  getBooks(@Param('id') id: string) {
+  getBook(@Param('id') id: string) {
     return this.booksService.getBook(id);
   }
 
   @Post()
-  createBook(
-    @Body('title') title: string,
-    @Body('author') author: string,
-    @Body('category') category: string,
-  ) {
-    return this.booksService.createBook(title, author, category);
+  // @UsePipes(ValidationPipe)
+  createBook(@Body() payload: CreateBookDto) {
+    return this.booksService.createBook(payload);
   }
 
   @Put('/:id')
-  updateBook(
-    @Param('id') id: string,
-    @Body('id') title: string,
-    @Body('id') author: string,
-    @Body('id') category: string,
-  ) {
-    return this.booksService.updateBook(id, title, author, category);
+  updateBook(@Param('id') id: string, @Body('id') payload: UpdateBookDto) {
+    return this.booksService.updateBook(id, payload);
   }
 
   @Delete('/:id')
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { FilterBookDto } from './dto/filter-book.dto';
 
 @Injectable()
 export class BooksService {
@@ -7,6 +10,28 @@ export class BooksService {
 
   getAllBook(): any[] {
     return this.books;
+  }
+  getBooks(filter: FilterBookDto): any[] {
+    const { title, author, category, min_year, max_year } = filter;
+    const books = this.books.filter((book) => {
+      if (title && book.title != title) {
+        return false;
+      }
+      if (author && book.author != author) {
+        return false;
+      }
+      if (category && book.category != category) {
+        return false;
+      }
+      if (min_year && book.min_year < min_year) {
+        return false;
+      }
+      if (max_year && book.max_year > max_year) {
+        return false;
+      }
+      return true;
+    });
+    return books;
   }
 
   // getBooks(title: string, author: string, category: string): any[] {
@@ -31,20 +56,24 @@ export class BooksService {
     return this.books[bookIdx];
   }
 
-  createBook(title: string, author: string, category: string) {
+  createBook(createBookDto: CreateBookDto) {
+    const { title, author, category, year } = createBookDto;
     this.books.push({
       id: uuidv4(),
       title,
       author,
       category,
+      year,
     });
   }
 
-  updateBook(id: string, title: string, author: string, category: string) {
+  updateBook(id: string, updateBookDto: UpdateBookDto) {
+    const { title, author, category, year } = updateBookDto;
     const bookIdx = this.findBookById(id);
     this.books[bookIdx].title = title;
     this.books[bookIdx].author = author;
     this.books[bookIdx].category = category;
+    this.books[bookIdx].year = year;
   }
 
   findBookById(id: string) {
